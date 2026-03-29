5d854db14d66f7ef5f882e2db2ac4966e6b16f06
VLBeauBot <vlefebvre21@protonmail.com>
2026-03-29 14:59:38 +0000

    v3 utils.py Heavy

---
+++ utils.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(f"add(2, 3) = {add(2, 3)}")
+    print(f"multiply(4, 5) = {multiply(4, 5)}")
